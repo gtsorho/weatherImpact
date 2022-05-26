cceebf75ecb78e6b2e7a037f5cb18eff47d80ce9
George TURKSON <georgesorho@gmail.com>
2022-05-26 16:14:24 +0000

    deplying

---
+++ sidebar.php
@@ -0,0 +1,50 @@
+
+
+<!-- Sidebar -->
+<div id="sidebar">
+    <div class="inner">
+
+        <!-- Search -->
+            <section id="search" class="alt">
+                <form method="post" action="#">
+                    <input type="text" name="query" id="query" placeholder="Search" />
+                </form>
+            </section>
+
+        <!-- Menu -->
+            <nav id="menu">
+                <header class="major">
+                    <h2>Menu</h2>
+                </header>
+                <ul>
+                    <li><a href="index.php">Homepage</a></li>
+                    <li><a href="http://aghub.miphost.com/">Ag Data hub</a></li>
+                    <li><a href="aghub.php">AGHUB Implementation</a></li>
+                    <li><a href="stakeholder.php">stakeholder Consultations</a></li>
+                    <li><a href="datasources.php">Existing Data Sources</a></li>
+                    <li><a href="existingapps.php">Existing Platform & Tools</a></li>
+                </ul>
+            </nav>
+
+        
+        <!-- Section -->
+            <section>
+                <header class="major">
+                    <h2>Overview</h2>
+                </header>
+                <p>The project, funded by the World Bank and led by IITA, seeks to scale the most strategic and impactful CGIAR Climate Change, Agriculture and Food Security (CCAFS)-Africa programs that promote resilience to climate change and improved food security. The focus is on six World Bank International Development Association (IDA)-eligible countries in Africa, including Ghana.</p>
+                <ul class="contact">
+                    <li class="icon solid fa-envelope"><a href="#">iita@cgiar.org</a></li>
+                    <li class="icon solid fa-phone">+233 20882 296</li>
+                    <li class="icon solid fa-home">CSIR Campus opp. Old Chinese Embassy Off Agostino Neto Road, <br />
+                    Airport Residential Area Accra, Ghana</li>
+                </ul>
+            </section>
+
+        <!-- Footer -->
+            <footer id="footer">
+                <p class="copyright">&copy; Untitled. All rights reserved. Demo Images: <a href="https://unsplash.com">Unsplash</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+            </footer>
+
+    </div>
+</div>
