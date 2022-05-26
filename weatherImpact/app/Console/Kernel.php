e5bd96730e6c0c2e329ae714c4a40ff9ab8c3edf
George TURKSON <georgesorho@gmail.com>
2022-05-26 15:55:53 +0000

    deploying

--- weatherImpact/app/Console/Kernel.php
+++ weatherImpact/app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('weather:daily')->dailyAt('11:00')->appendOutputTo('scheduler.log');
+        
     }
 
     /**
